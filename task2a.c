097ce43ab6e1506e4e8413e177a5f2009fa23a58
akashm-140 <amandapa@terpmail.umd.edu>
2024-09-18 14:41:13 -0400

    task2a.c

--- task2a.c
+++ task2a.c
@@ -1,27 +1,27 @@
-//Task 2a
-//#include <stdio.h>
-//
-//int main() {
-//    char str[100];   // Array to hold the input string
-//    char ch;         // Character to search for
-//    int count = 0;   // Counter for occurrences
-//
-//    // Prompt the user for input
-//    printf("Enter a string: ");
-//    scanf("%s", str);
-//
-//    printf("Enter a character to count: ");
-//    scanf(" %c", &ch);  // Note the space before %c to consume any leftover newline character
-//
-//    // Loop through the string to count occurrences of the character
-//    for (int i = 0; str[i] != '\0'; i++) {
-//        if (str[i] == ch) {
-//            count++;
-//        }
-//    }
-//
-//    // Print the result
-//    printf("%c appears %d time%s\n", ch, count, count == 1 ? "" : "s");
-//
-//    return 0;
-//}
+Task 2a
+#include <stdio.h>
+
+int main() {
+   char str[100];   // Array to hold the input string
+   char ch;         // Character to search for
+   int count = 0;   // Counter for occurrences
+
+   // Prompt the user for input
+   printf("Enter a string: ");
+   scanf("%s", str);
+
+   printf("Enter a character to count: ");
+   scanf(" %c", &ch);  // Note the space before %c to consume any leftover newline character
+
+   // Loop through the string to count occurrences of the character
+   for (int i = 0; str[i] != '\0'; i++) {
+       if (str[i] == ch) {
+           count++;
+       }
+   }
+
+   // Print the result
+   printf("%c appears %d time%s\n", ch, count, count == 1 ? "" : "s");
+
+   return 0;
+}
